5ef1241d79db89df2aa7acf4de5f585d81281d8a
Justin Ho <jho.main.go@gmail.com>
2022-12-01 11:10:35 -0500

    working discord connection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import nacl from 'tweetnacl';
+const Buffer = require('buffer/').Buffer;
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
-	},
-};
+async function handleRequest(request) {
+  if (request.method === 'POST') {
+    const req = await request.json()
+
+    const headers = request.headers
+    const PUBLIC_KEY = 'cd677acec866ef0eb5879773bdcb9bb4c27f283fb96d1d97428c733bf4545929'
+    const signature = headers.get('X-Signature-Ed25519')
+    const timestamp = headers.get('X-Signature-Timestamp')
+
+    if (signature && timestamp) {
+      const isVerified = nacl.sign.detached.verify(
+        Buffer(timestamp + JSON.stringify(req)),
+        Buffer(signature, 'hex'),
+        Buffer(PUBLIC_KEY, 'hex'),
+      )
+
+      if (!isVerified) {
+        return new Response(JSON.stringify(req), { status: 401 })
+      } else {
+        return new Response(JSON.stringify(req), { status: 200 })
+      }
+    }
+  }
+
+	if (request.method === 'GET') {
+		return new Response('Hello World!', { status: 200 })
+	}
+}
+
+addEventListener('fetch', event => {
+	return event.respondWith((handleRequest(event.request)))
+})
