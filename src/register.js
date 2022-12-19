bfec5b0d1b7a1e8d847742f7cb8bf8575d6525e2
Justin Ho <jho.main.go@gmail.com>
2022-12-19 00:49:50 -0500

    working register

--- src/register.js
+++ src/register.js
@@ -1,15 +1,28 @@
-export async function registerCommands(env) {
-  let global_url = `https://discord.com/api/v10/applications/${env.DISCORD_CLIENT_ID}/commands`;
-  let guild_url = `https://discord.com/api/v8/applications/${env.DISCORD_CLIENT_ID}/guilds/${env.DISCORD_GUILD}/commands`;
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID, DISCORD_GUILD_ID } = process.env
+const tk = require('./commands/tk')
 
-  await fetch(guild_url, {
-    body: JSON.stringify({
-    
-    }),
-    method: POST,
-    headers: {
-      'Content-type': 'application/json',
-      'Authorization': `Bearer ${(await getClientCredentialToken())}`
-    }
-  })
+const global_url = `https://discord.com/api/v10/applications/${DISCORD_CLIENT_ID}/commands`;
+const guild_url = `https://discord.com/api/v10/applications/${DISCORD_CLIENT_ID}/guilds/${DISCORD_GUILD_ID}/commands`;
+
+async function registerSlashCommands(url, body) {
+	const response = await fetch(url, {
+		body: JSON.stringify(body),
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization' : `Bot ${DISCORD_TOKEN}`
+		}
+	})
+
+	if (response.status != '200'){
+		return response.json()
+	}
+	return response.json()
 }
+
+registerSlashCommands(guild_url, tk).then((response) => {
+	console.log(response)
+	// console.log(`Name: ${response.name}, Id: ${response.id}`)
+}).catch((error) => {
+	console.log("ERROR!: \n", error)
+})
